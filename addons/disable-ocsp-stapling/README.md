cf8e1fc61ce7fe031d1acd4874ca8dfe919a5e4f
EKR <ekr@rtfm.com>
2017-05-30 12:40:23 -0700

    Add an add-on to disable OCSP stapling

---
+++ addons/disable-ocsp-stapling/README.md
@@ -0,0 +1,2 @@
+Disable OCSP Stapling to temporarily fix:
+https://bugzilla.mozilla.org/show_bug.cgi?id=1368433
---
+++ addons/disable-ocsp-stapling/bootstrap.js
@@ -0,0 +1,40 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+let {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+Cu.import("resource://gre/modules/Preferences.jsm");
+
+const PREF = "security.ssl.enable_ocsp_stapling";
+let gStarted = false;
+
+async function startup(data, reason) {
+  // Don't do anything if the user has already messed with this
+  // setting.
+  let userprefs = new Preferences();
+  if (userprefs.isSet(PREF)) {
+    console.log("User has modified OCSP stapling. Skipping");
+    return;
+  }
+  
+  // Seems startup() function is launched twice after install, we're
+  // unsure why so far. We only want it to run once.
+  if (gStarted) {
+    debug("Attempt to call startup twice. reason="+reason);
+    return;
+  }
+
+  let prefs = new Preferences({ defaultBranch: true });
+  prefs.set(PREF, false);
+}
+
+function shutdown() {
+  gStarted = false;
+}
+
+function install() {}
+function uninstall() {}
+
